da10ad3ce964da3e869049e31335a2ae359af4f7
Illunation <76393506+Unknown-cmd-systemcracker@users.noreply.github.com>
2022-11-29 16:17:33 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    private static long[] fibonacciCache;
+
+    public static void main(String[] args) {
+        // ( 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 )
+
+        int n = 1; // <-- Put here the numbers of how many fibonacci-arrays should be iterated.
+
+        fibonacciCache = new long[n+1];
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println(fibonacci(i) + " ");
+        }
+
+    }
+    private static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        if (fibonacciCache[n] != 0) {
+            return fibonacciCache[n];
+        }
+
+        long nthFibonacciNumber = (fibonacci(n-1) + fibonacci(n-2));
+        fibonacciCache[n] = nthFibonacciNumber;
+        return nthFibonacciNumber;
+    }
+}
